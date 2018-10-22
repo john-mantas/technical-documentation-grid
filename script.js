74d77f326cda4caa44524a40e3fbe2e63979760c
John Mantas <mantasjohn67@gmail.com>
2018-10-22 21:27:27 +0300

    feat: Make the navbar responsive

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const DOC_HEADER_LINKS = document.getElementById('doc_header_links');
 DOC_HEADER_LINKS.addEventListener('click', (e)=> {
   DOC_HEADER_LINKS.classList.toggle('is-open');
 });
+
+const NAVBAR = document.getElementById('navbar');
+
+NAVBAR.addEventListener('click', ()=> {
+  NAVBAR.classList.toggle('is-open');
+});
