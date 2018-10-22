2dc513d15846f5a30df968fe5e56f82a2071f02b
John Mantas <mantasjohn67@gmail.com>
2018-10-22 21:32:45 +0300

    refactor: Clean unnecessary code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DOC_HEADER_LINKS = document.getElementById('doc_header_links');
 
-DOC_HEADER_LINKS.addEventListener('click', (e)=> {
+DOC_HEADER_LINKS.addEventListener('click', ()=> {
   DOC_HEADER_LINKS.classList.toggle('is-open');
 });
 
