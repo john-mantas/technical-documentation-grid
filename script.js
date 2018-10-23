b0542ad359d951f68e204076c1b0d654f11edef0
John Mantas <mantasjohn67@gmail.com>
2018-10-23 23:58:27 +0300

    fix: Make general improvements

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ DOC_HEADER_LINKS.addEventListener('click', ()=> {
 });
 
 const NAVBAR = document.getElementById('navbar');
+const NAVBAR_BTN = document.getElementById('navbar_btn');
 
 NAVBAR.addEventListener('click', ()=> {
   NAVBAR.classList.toggle('is-open');
 });
+
+NAVBAR_BTN.addEventListener('click', ()=> {
+  NAVBAR.classList.toggle('is-open');
+});
+
+function getTop() {
+  let doc_header_height = document.getElementById('doc-header').clientHeight;
+  document.getElementById('main-doc').style.top = `${doc_header_height}px`;
+  document.getElementById('main-doc').style.height = `calc(100vh - ${doc_header_height}px)`;
+};
+
+window.onload = getTop;
+window.onresize = getTop;
